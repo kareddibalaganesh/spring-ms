258970d988a929f46652a16bf7237cb89996b240
srinfotechbatch3 <srinfotechbatch3@gmail.com>
2025-09-18 16:07:11 +0530

    Update HelloController.java

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Welcome to SRINFOTECH SOlutions Private Limited ANd AWS & DEVOPS Training Batch3!";
+		return "Welcome to SRINFOTECH SOlutions Private Limited ANd AWS & DEVOPS Training!";
 	}
 
 }
